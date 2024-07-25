11792f2282434b7e887322b2a13fd31ec2ac60a5
erraticframedev <erraticframe@gmail.com>
2024-07-25 16:52:07 +0200

    Hello

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, Fragment, useEffect } from 'react'
 import './App.css'
+import { createApi } from "unsplash-js"
 
-function App() {
-  const [count, setCount] = useState(0)
+const api = createApi({
+  accessKey: "e1uyUwS-ZeVJucBPfEHg1tKtwYpH5apYxBCWZI1fBkY"
+});
+
+
+
+const PhotoComp = ({ photo }: any) => {
+  const { user, urls } = photo;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+
+    <Fragment>
+
+      <a href="https://react.dev" target="_blank">
+        <div className="card" >
+          <img className="img" src={urls.regular} />
+        </div>
+      </a>
+      <a
+        className="credit"
+        target="_blank"
+        href={`https://unsplash.com/@${user.username}`}
+      >
+        <p className="read-the-docs">
+          {user.name}
         </p>
+      </a>
+
+    </Fragment>
+  );
+};
+
+function App() {
+
+  const [data, setPhotosResponse] = useState(Object);
+
+  api.users.getPhotos
+  useEffect(() => {
+    api.users
+      .getPhotos({
+        username: "erraticframe",
+        orientation: "landscape",
+        stats: true
+      })
+      .then(result => {
+        setPhotosResponse(result);
+      })
+      .catch(() => {
+        console.log("something went wrong!");
+      });
+  }, []);
+
+
+  if (data === null) {
+    return <div>Loading...</div>;
+  } else if (data.errors) {
+    return (
+      <div>
+        <div>{data.errors[0]}</div>
+        <div>PS: Make sure to set your access token!</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  } else {
+    return (
+      <div className="feed">
+        <ul className="columnUl">
+          {data.response.results.map(({photo}:any) => (
+            <li key={photo.id} className="li">
+              <PhotoComp photo={photo} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+
 }
 
 export default App
